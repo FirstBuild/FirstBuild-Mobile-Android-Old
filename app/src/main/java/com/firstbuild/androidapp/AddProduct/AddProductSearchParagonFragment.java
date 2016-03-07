0d53999cf57dd7c7620704e67e62fb86600f7548
wowHollis <wowhos@gmail.com>
2016-03-07 14:26:18 +0900

    clean code.

--- app/src/main/java/com/firstbuild/androidapp/AddProduct/AddProductSearchParagonFragment.java
+++ app/src/main/java/com/firstbuild/androidapp/AddProduct/AddProductSearchParagonFragment.java
@@ -6,10 +6,7 @@ import android.app.Fragment;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGattService;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -21,7 +18,6 @@ import android.widget.ImageView;
 
 import com.firstbuild.androidapp.ParagonValues;
 import com.firstbuild.androidapp.R;
-import com.firstbuild.androidapp.productManager.ProductInfo;
 import com.firstbuild.androidapp.productManager.ProductManager;
 import com.firstbuild.commonframework.bleManager.BleListener;
 import com.firstbuild.commonframework.bleManager.BleManager;
@@ -35,15 +31,146 @@ import java.util.Map;
  * A simple {@link Fragment} subclass.
  */
 public class AddProductSearchParagonFragment extends Fragment {
+    private final int SPINNING_REPEAT_COUNT = 30;
     private String TAG = AddProductActivity.class.getSimpleName();
     private ImageView spinningImage;
     private RotateAnimation spinningAnimation;
     private String deviceAddress = null;
     private AddProductActivity attached = null;
+    private BleListener bleListener = new BleListener() {
+        @Override
+        public void onScanDevices(HashMap<String, BluetoothDevice> bluetoothDevices) {
+            super.onScanDevices(bluetoothDevices);
+
+            Log.d(TAG, "onScanDevices IN");
+
+            Log.d(TAG, "bluetoothDevices size: " + bluetoothDevices.size());
+            for (Map.Entry<String, BluetoothDevice> entry : bluetoothDevices.entrySet()) {
+
+                // Retrieves address and name
+                BluetoothDevice device = entry.getValue();
+                deviceAddress = device.getAddress();
+                String name = device.getName();
+                int bondState = device.getBondState();
+
+                Log.d(TAG, "------------------------------------");
+                Log.d(TAG, "Device address: " + deviceAddress);
+                Log.d(TAG, "Device name: " + name);
+                Log.d(TAG, "Device Bond state: " + device.getBondState());
+
+
+                if (ParagonValues.TARGET_DEVICE_NAME.equals(name)) {
+                    Log.d(TAG, "device found: " + device.getName());
+                    boolean isFound = false;
+
+
+                    if (bondState == device.BOND_NONE) {
+                        Log.d(TAG, "device not bonded: " + bondState);
+                        // Pair to device
+                        BleManager.getInstance().pairDevice(device);
+                    }
+                    else if (bondState == device.BOND_BONDED || bondState == device.BOND_BONDING) {
+                        Log.d(TAG, "device bonded: " + bondState);
+
+                        if (ProductManager.getInstance().getProductByAddress(deviceAddress) == null) {
+                            isFound = true;
+                        }
+                    }
+                    else {
+                        Log.d(TAG, "device bonded or bonding state: " + bondState);
+
+                        // This part must be replace with proper warning page
+                        goToErrorScreen();
+                    }
+
+
+                    if (isFound) {
+                        // Stop ble device scanning
+                        BleManager.getInstance().stopScan();
+
+                        // case for Paragon Master already paired but not in dashboard.
+                        attached.setNewProductAddress(deviceAddress);
+
+                        // Transit to success UI
+                        getFragmentManager().
+                                beginTransaction().
+                                replace(R.id.content_frame, new AddProductFoundParagonFragment()).
+                                addToBackStack(null).
+                                commit();
+
+                        break;
+                    }
+                }
+            }
+            Log.d(TAG, "====================================");
+        }
+
+        @Override
+        public void onScanStateChanged(int status) {
+            super.onScanStateChanged(status);
+
+            Log.d(TAG, "[onScanStateChanged] status: " + status);
+
+            if (status == BleValues.START_SCAN) {
+                Log.d(TAG, "Scanning BLE devices");
+            }
+            else {
+                Log.d(TAG, "Stop scanning BLE devices");
+            }
+        }
+
+        @Override
+        public void onConnectionStateChanged(final String address, final int status) {
+            super.onConnectionStateChanged(address, status);
+
+            Log.d(TAG, "[onConnectionStateChanged] address: " + address + ", status: " + status);
+        }
+
+        @Override
+        public void onServicesDiscovered(String address, List<BluetoothGattService> bleGattServices) {
+            super.onServicesDiscovered(address, bleGattServices);
+
+            Log.d(TAG, "[onServicesDiscovered] address: " + address);
+        }
+
+        @Override
+        public void onCharacteristicRead(String address, String uuid, byte[] value) {
+            super.onCharacteristicRead(address, uuid, value);
+
+            Log.d(TAG, "[onCharacteristicRead] address: " + address + ", uuid: " + uuid);
+
+        }
+    };
 
     public AddProductSearchParagonFragment() {
         // Required empty public constructor
         Log.d(TAG, "AddProductSearchParagonFragment IN");
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult - requestCode: " + requestCode);
+        Log.d(TAG, "onActivityResult - resultCode: " + resultCode);
+
+        if (requestCode == BleValues.REQUEST_ENABLE_BT) {
+            if (resultCode == -1) {
+                // Success
+                Log.d(TAG, "Bluetooth adapter is enabled. Start scanning.");
+                spinningAnimation.setRepeatCount(SPINNING_REPEAT_COUNT);
+                BleManager.getInstance().startScan();
+            }
+            else if (resultCode == 0) {
+                Log.d(TAG, "Bluetooth adapter is still disabled");
+                getFragmentManager().
+                        beginTransaction().
+                        replace(R.id.content_frame, new AddProductSelectFragment()).
+                        addToBackStack(null).
+                        commit();
+            }
+            else {
+                // Else
+            }
+        }
     }
 
     @Override
@@ -53,20 +180,13 @@ public class AddProductSearchParagonFragment extends Fragment {
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        attached = null;
-        BleManager.getInstance().removeListener(bleListener);
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_product_searching_paragon, container, false);
 
         // Set and start spinning image
-        spinningImage = (ImageView)view.findViewById(R.id.imgSpinner);
+        spinningImage = (ImageView) view.findViewById(R.id.imgSpinner);
         spinningImage.setAnimation(makeAnimation());
 
         // Initialize ble manager
@@ -94,35 +214,21 @@ public class AddProductSearchParagonFragment extends Fragment {
         }
         else {
             Log.d(TAG, "Bluetooth adapter is already enabled. Start scanning.");
-            spinningAnimation.setRepeatCount(30);
+            spinningAnimation.setRepeatCount(SPINNING_REPEAT_COUNT);
             BleManager.getInstance().startScan();
         }
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        Log.d(TAG, "onActivityResult - requestCode: " + requestCode);
-        Log.d(TAG, "onActivityResult - resultCode: " + resultCode);
+    public void onSaveInstanceState(Bundle outState) {
 
-        if (requestCode == BleValues.REQUEST_ENABLE_BT) {
-            if (resultCode == -1) {
-                // Success
-                Log.d(TAG, "Bluetooth adapter is enabled. Start scanning.");
-                spinningAnimation.setRepeatCount(30);
-                BleManager.getInstance().startScan();
-            }
-            else if(resultCode == 0){
-                Log.d(TAG, "Bluetooth adapter is still disabled");
-                getFragmentManager().
-                        beginTransaction().
-                        replace(R.id.content_frame, new AddProductSelectFragment()).
-                        addToBackStack(null).
-                        commit();
-            }
-            else{
-                // Else
-            }
-        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        attached = null;
+        BleManager.getInstance().removeListener(bleListener);
     }
 
     private RotateAnimation makeAnimation() {
@@ -154,116 +260,7 @@ public class AddProductSearchParagonFragment extends Fragment {
         return spinningAnimation;
     }
 
-    private BleListener bleListener = new BleListener() {
-        @Override
-        public void onScanDevices(HashMap<String, BluetoothDevice> bluetoothDevices) {
-            super.onScanDevices(bluetoothDevices);
-
-            Log.d(TAG, "onScanDevices IN");
-
-            Log.d(TAG, "bluetoothDevices size: " + bluetoothDevices.size());
-            for (Map.Entry<String, BluetoothDevice> entry : bluetoothDevices.entrySet()) {
-
-                // Retrieves address and name
-                BluetoothDevice device = entry.getValue();
-                deviceAddress = device.getAddress();
-                String name = device.getName();
-                int bondState = device.getBondState();
-
-                Log.d(TAG, "------------------------------------");
-                Log.d(TAG, "Device address: " + deviceAddress);
-                Log.d(TAG, "Device name: " + name);
-                Log.d(TAG, "Device Bond state: " +device.getBondState());
-
-
-                if (ParagonValues.TARGET_DEVICE_NAME.equals(name)){
-                    Log.d(TAG, "device found: " + device.getName());
-                    boolean isFound = false;
-
-
-                    if(bondState == device.BOND_NONE) {
-                        Log.d(TAG, "device not bonded: " + bondState);
-                        // Pair to device
-                        BleManager.getInstance().pairDevice(device);
-                    }
-                    else if(bondState == device.BOND_BONDED || bondState == device.BOND_BONDING){
-                        Log.d(TAG, "device bonded: " + bondState);
-
-                        if(ProductManager.getInstance().getProductByAddress(deviceAddress) == null){
-                            isFound = true;
-                        }
-                    }
-                    else{
-                        Log.d(TAG, "device bonded or bonding state: " + bondState);
-
-                        // This part must be replace with proper warning page
-                        goToErrorScreen();
-                    }
-
-
-                    if(isFound){
-                        // Stop ble device scanning
-                        BleManager.getInstance().stopScan();
-
-                        // case for Paragon Master already paired but not in dashboard.
-                        attached.setNewProductAddress(deviceAddress);
-
-                        // Transit to success UI
-                        getFragmentManager().
-                                beginTransaction().
-                                replace(R.id.content_frame, new AddProductFoundParagonFragment()).
-                                addToBackStack(null).
-                                commit();
-
-                        break;
-                    }
-                }
-            }
-            Log.d(TAG, "====================================");
-        }
-
-        @Override
-        public void onScanStateChanged(int status) {
-            super.onScanStateChanged(status);
-
-            Log.d(TAG, "[onScanStateChanged] status: " + status);
-
-            if (status == BleValues.START_SCAN) {
-                Log.d(TAG, "Scanning BLE devices");
-            } else {
-                Log.d(TAG, "Stop scanning BLE devices");
-            }
-        }
-
-        @Override
-        public void onConnectionStateChanged(final String address, final int status) {
-            super.onConnectionStateChanged(address, status);
-
-            Log.d(TAG, "[onConnectionStateChanged] address: " + address + ", status: " + status);
-        }
-
-        @Override
-        public void onServicesDiscovered(String address, List<BluetoothGattService> bleGattServices) {
-            super.onServicesDiscovered(address, bleGattServices);
-
-            Log.d(TAG, "[onServicesDiscovered] address: " + address);
-        }
-
-        @Override
-        public void onCharacteristicRead(String address, String uuid, byte[] value) {
-            super.onCharacteristicRead(address, uuid, value);
-
-            Log.d(TAG, "[onCharacteristicRead] address: " + address + ", uuid: " + uuid);
-            
-        }
-    };
-
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-
-    }
-
-    private void goToErrorScreen(){
+    private void goToErrorScreen() {
         getFragmentManager().
                 beginTransaction().
                 replace(R.id.content_frame, new AddProductConnectionErrorFragment()).
